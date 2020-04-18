e5e2327e75f4e78c0ebab10cdda251b0f346945b
Evan Burton <evan@heyevan.com>
2020-04-18 08:20:55 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,16 @@
 def prime?(integer)
-    if integer > 1
-      range_to_test = (2..integer-1)
-      range_to_test.all? do |test_number|
+    if integer > 1 #establishes negative numbers cannot be prime. If negative, return false.
+      (2..integer-1).all? do |test_number|
         integer % test_number != 0
       end
     else
       false
     end
   end
+
+iterates over 2 to
+
+  2 / 2 = 1
+
+  2 / 1 = .5
+  returns false
